ab93b8a0020ba720f7e0b41897c9f816497b1a99
Mialalala1 <89511273+Mialalala1@users.noreply.github.com>
2021-10-12 00:41:02 +0800

    Update Alpha.sol

--- contracts/Alpha.sol
+++ contracts/Alpha.sol
@@ -5,20 +5,21 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
+// 需要去除mint限制
+// erc20 token staking based PASS contract. User stake creator's erc20 tokens to mint PASS and burn PASS to get erc20 tokens back.
 contract Alpha is Context, ERC721 {
     using Counters for Counters.Counter;
 
     event Mint(address indexed from, uint256 indexed tokenId);
     event Burn(address indexed from, uint256 indexed tokenId);
 
-    address public erc20;
-    uint256 public rate;
-
+    address public erc20;       // creator's erc20 token address
+    uint256 public rate;        // staking rate of erc20 token/PASS
+    // token id counter. For erc721 contract, PASS serial number = token id
     Counters.Counter private tokenIdTracker = Counters.Counter({
         _value: 1
     });
-    mapping(address => uint256) private holders;
+//    mapping(address => uint256) private holders;    // check if user minted PASS to limit that only origial minter can burn the same PASS
 
     constructor(string memory _name, string memory _symbol, address _erc20, uint256 _rate) ERC721(_name, _symbol) {
         erc20 = _erc20;
@@ -26,22 +27,22 @@ contract Alpha is Context, ERC721 {
     }
 
     function mint() public returns (uint256 tokenId) {
-        require(holders[_msgSender()] == 0, "Alpha: sender has been holder");
+//        require(holders[_msgSender()] == 0, "error: sender has been holder");   
 
-        tokenId = tokenIdTracker.current();
-        IERC20(erc20).transferFrom(_msgSender(), address(this), rate);
-        _safeMint(_msgSender(), tokenId);
-        holders[_msgSender()] = tokenId;
+        tokenId = tokenIdTracker.current();                             // accumulate the token id
+        IERC20(erc20).transferFrom(_msgSender(), address(this), rate);  // user stake erc20 tokens into contract
+        _safeMint(_msgSender(), tokenId);                               // mint PASS to user address
+//        holders[_msgSender()] = tokenId;                                // associate the user address with token id 
 
         emit Mint(_msgSender(), tokenId);
 
-        tokenIdTracker.increment();
+        tokenIdTracker.increment();                                     // automate token id increment
     }
-
+    // 需要解除holder的限制，不需要限制只有某个PASS的原始拥有者才能销毁。
     function burn(uint256 tokenId) public {
-        require(tokenId != 0 && tokenId == holders[_msgSender()], "Alpha: sender is not holder");
-
-        delete holders[_msgSender()];
+//        require(tokenId != 0 && tokenId == holders[_msgSender()], "error: sender is not holder");   
+        require(tokenId != 0, "error: token id cannot be zero");
+//        delete holders[_msgSender()];
         _burn(tokenId);
         IERC20(erc20).transfer(_msgSender(), rate);
 
