7d76c6e0213eee613fe229df5823030f84373e13
Mialalala1 <89511273+Mialalala1@users.noreply.github.com>
2021-10-15 18:24:55 +0800

    Update and rename Gamma.sol to FixedPrice.sol

--- contracts/FixedPrice.sol
+++ contracts/FixedPrice.sol
@@ -7,19 +7,18 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol"; 
 
-// 需要修改合约增加总量的限制，去除mint限制
 // fixed price PASS contract. Users pay specific erc20 tokens to purchase PASS from creator DAO
-contract Gamma is Context, ERC721, Ownable {
+contract FixedPrice is Context, ERC721, Ownable {
     using Counters for Counters.Counter;
     using Strings for uint256;
 
     event Mint(address indexed from, uint256 indexed tokenId);
     event Withdraw(address indexed to, uint256 amount);
 
-    address public currentowner;       // contract owner/admin is normally the creator
+    address public owner;       // contract owner is normally the creator
     address public erc20;       // erc20 token used to purchase PASS
     uint256 public rate;        // price rate of erc20 tokens/PASS
-    uint256 public maxSupply; // Maximum supply
+    uint256 public maxSupply;   // Maximum supply of PASS
     
     // Optional mapping for token URIs
     mapping (uint256 => string) private _tokenURIs;
@@ -31,10 +30,9 @@ contract Gamma is Context, ERC721, Ownable {
     Counters.Counter private tokenIdTracker = Counters.Counter({
         _value: 1
     });
-//    mapping(address => uint256) private holders;    // check if user minted PASS to limit that each address can only mint one PASS in the contract
 
     constructor(string memory _name, string memory _symbol, string memory _bURI, address _erc20, uint256 _rate, uint256 _maxSupply) ERC721(_name, _symbol) {
-        currentowner = tx.origin;      // the creator of DAO will be the owner/admin of PASS contract
+        owner = tx.origin;      // the creator of DAO will be the owner of PASS contract
         _baseURIextended = _bURI;
         erc20 = _erc20;         
         rate = _rate;
@@ -42,9 +40,7 @@ contract Gamma is Context, ERC721, Ownable {
     }
     
     function setBaseURI(string memory baseURI_) public {
-
-        require(currentowner == _msgSender(), "Error: must have admin role");  // only contract owner/admin can setTokenURI
-
+        require(owner == _msgSender(), "FixedPrice: caller is not the owner");  // only contract owner can setTokenURI
         _baseURIextended = baseURI_;
     }
 
@@ -76,38 +72,31 @@ contract Gamma is Context, ERC721, Ownable {
     }
 
     function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
-
-        require(currentowner == _msgSender(), "Error: must have admin role");  // only contract owner/admin can setTokenURI
-
+        require(owner == _msgSender(), "FixedPrice: caller is not the owner");  // only contract owner can setTokenURI
         _setTokenURI(tokenId, _tokenURI);
     }
 
     // user buy PASS from contract with specific erc20 tokens
     function mint() public returns (uint256 tokenId) {
-//        require(holders[_msgSender()] == 0, "error: each user can only mint once");   // each user can only mint once in this contract
-        require((tokenIdTracker.current() <= maxSupply), "Error: exceeds maximum supply");
+        require((tokenIdTracker.current() <= maxSupply), "FixedPrice: exceeds maximum supply");
         tokenId = tokenIdTracker.current();                             // accumulate the token id
 
-        // IERC20(erc20).transferFrom(_msgSender(), address(this), rate);  // send erc20 tokens from user to contract
         bool success = IERC20(erc20).transferFrom(_msgSender(), address(this), rate);
-        require(success, "Transfer failed.");
+        // in case old contract only return false when transfer fails
+        require(success, "ERC20: Transfer failed.");
 
         _safeMint(_msgSender(), tokenId);                               // mint PASS to user address
-//        holders[_msgSender()] = tokenId;                                // associate the user address with token id
-
         emit Mint(_msgSender(), tokenId);
-
-        _setTokenURI(tokenId, tokenId.toString());
 
         tokenIdTracker.increment();                                     // automate token id increment
     }
 
-    // contract owner/admin withdraw erc20 tokens from contract
+    // owner withdraw erc20 tokens from contract
     function withdraw() public {
-        require(currentowner == _msgSender(), "Error: must have admin role");  // only contract owner/admin can withdraw reserve of erc20 tokens
-        uint256 amount = IERC20(erc20).balanceOf(address(this));        // get the amount of erc20 tokens reserved in contract
-        IERC20(erc20).transfer(_msgSender(), amount);                   // transfer erc20 tokens to contract owner address
+        require(owner == _msgSender(), "FixedPrice: caller is not the owner");  // only contract owner can withdraw reserve of erc20 tokens
 
+        uint256 amount = IERC20(erc20).balanceOf(address(this));                // get the amount of erc20 tokens reserved in contract
+        IERC20(erc20).transfer(_msgSender(), amount);                           // transfer erc20 tokens to contract owner address
         emit Withdraw(_msgSender(), amount);
     }
 
