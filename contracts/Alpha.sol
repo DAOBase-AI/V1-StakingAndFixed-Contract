d73f411470c54ac0a1948207fcbc09dd58c55ad9
Mialalala1 <89511273+Mialalala1@users.noreply.github.com>
2021-10-15 16:50:15 +0800

    Update Alpha.sol

--- contracts/Alpha.sol
+++ contracts/Alpha.sol
@@ -7,16 +7,15 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol"; 
 
-// 需要去除mint限制
 // erc20 token staking based PASS contract. User stake creator's erc20 tokens to mint PASS and burn PASS to get erc20 tokens back.
-contract Alpha is Context, ERC721, Ownable {
+contract TokenBase is Context, ERC721, Ownable {
     using Counters for Counters.Counter;
     using Strings for uint256;
 
     event Mint(address indexed from, uint256 indexed tokenId);
     event Burn(address indexed from, uint256 indexed tokenId);
 
-    address public currentowner;       // contract owner/admin is normally the creator
+    address public owner;       // contract owner/admin is normally the creator
     address public erc20;       // creator's erc20 token address
     uint256 public rate;        // staking rate of erc20 token/PASS
 
@@ -30,19 +29,16 @@ contract Alpha is Context, ERC721, Ownable {
     Counters.Counter private tokenIdTracker = Counters.Counter({
         _value: 1
     });
-    //    mapping(address => uint256) private holders;    // check if user minted PASS to limit that only origial minter can burn the same PASS
-
+ 
     constructor(string memory _name, string memory _symbol, string memory _bURI, address _erc20, uint256 _rate) ERC721(_name, _symbol) {
-        currentowner = tx.origin;      // the creator of DAO will be the owner/admin of PASS contract
+        owner = tx.origin;      // the creator of DAO will be the owner/admin of PASS contract
         _baseURIextended = _bURI;
         erc20 = _erc20;
         rate = _rate;
     }
 
     function setBaseURI(string memory baseURI_) public {
-
-        require(currentowner == _msgSender(), "Error: must have admin role");  // only contract owner/admin can setTokenURI
-
+        require(owner == _msgSender(), "TokenBase: must have admin role");  // only contract owner/admin can setTokenURI
         _baseURIextended = baseURI_;
     }
 
@@ -68,44 +64,29 @@ contract Alpha is Context, ERC721, Ownable {
         return string(abi.encodePacked(base, tokenId.toString()));
     }
 
-    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
-        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
+        require(owner == _msgSender(), "TokenBase: must have admin role");  // only contract owner/admin can setTokenURI
+        require(_exists(tokenId), "ERC721: URI set of nonexistent token");
         _tokenURIs[tokenId] = _tokenURI;
     }
 
-    function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
+    function mint() public returns (uint256 tokenId) { 
+        tokenId = tokenIdTracker.current();                                // accumulate the token id
 
-        require(currentowner == _msgSender(), "Error: must have admin role");  // only contract owner/admin can setTokenURI
-
-        _setTokenURI(tokenId, _tokenURI);
-    }
-
-    function mint() public returns (uint256 tokenId) {
-//        require(holders[_msgSender()] == 0, "error: sender has been holder");   
-
-        tokenId = tokenIdTracker.current();                             // accumulate the token id
-        // IERC20(erc20).transferFrom(_msgSender(), address(this), rate);  // user stake erc20 tokens into contract
         bool success = IERC20(erc20).transferFrom(_msgSender(), address(this), rate);
-        require(success, "Transfer failed.");
+        require(success, "ERC20: transfer failed.");
 
-        _safeMint(_msgSender(), tokenId);                               // mint PASS to user address
-//        holders[_msgSender()] = tokenId;                                // associate the user address with token id 
-
+        _safeMint(_msgSender(), tokenId);                                 // mint PASS to user address
         emit Mint(_msgSender(), tokenId);
 
-        _setTokenURI(tokenId, tokenId.toString());
-
-        tokenIdTracker.increment();                                     // automate token id increment
+        tokenIdTracker.increment();                                       // automate token id increment
     }
 
-    // 需要解除holder的限制，不需要限制只有某个PASS的原始拥有者才能销毁。
-    function burn(uint256 tokenId) public {
-//        require(tokenId != 0 && tokenId == holders[_msgSender()], "error: sender is not holder");   
-        require(tokenId != 0, "Error: token id cannot be zero");
-//        delete holders[_msgSender()];
+   function burn(uint256 tokenId) public {
+        require(tokenId != 0, "TokenBase: token id cannot be zero");
+ 
         _burn(tokenId);
         IERC20(erc20).transfer(_msgSender(), rate);
-
         emit Burn(_msgSender(), tokenId);
     }
 
