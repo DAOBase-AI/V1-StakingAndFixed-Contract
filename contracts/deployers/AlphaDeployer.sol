266dbf75a56c0c74358e4397486b0740c0444583
Mialalala1 <89511273+Mialalala1@users.noreply.github.com>
2021-10-15 15:02:01 +0800

    update contract and function name

--- contracts/deployers/AlphaDeployer.sol
+++ contracts/deployers/AlphaDeployer.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "../Alpha.sol";
+import "../TokenBase.sol";
 
-contract AlphaDeployer {
-    function deployAlpha(
+contract TokenBaseDeployer {
+    function deployTokenBase(
         string memory _name,
         string memory _symbol,
         string memory _bURI,
         address _erc20,
         uint256 _rate
     ) public returns (address) {
-        Alpha alpha = new Alpha(_name, _symbol, _bURI, _erc20, _rate);
-        address addr = address(alpha);
+        TokenBase tokenBase = new TokenBase(_name, _symbol, _bURI, _erc20, _rate);
+        address addr = address(tokenBase);
         return addr;
     }
 }
