c759f80a5d998de4d044d0ac4450db80d4963a68
72521 <ysl47698@gmail.com>
2021-12-08 18:35:04 +0800

    feat: add timelock for setPlatformParms

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -8,54 +8,63 @@ import "./interfaces/IFixedPeriodDeployer.sol";
 import "./interfaces/IFixedPriceDeployer.sol";
 
 contract Factory is Ownable {
-  address private tokenBaseDeployer;    // staking erc20 tokens to mint PASS
-  address private nftBaseDeployer;      // staking erc721 tokens to mint PASS
-  address private fixedPeriodDeployer;  // pay erc20 tokens to mint PASS in a fixed period with linearly decreasing price
-  address private fixedPriceDeployer;   // pay erc20 tokens to mint PASS with fixed price
+  uint256 public immutable COOLDOWN_SECONDS = 2 days;
 
-  address payable public platform;     // The PASS platform commission account
-  uint256 public platformRate;         // The PASS platform commission rate in pph
+  /// @notice Seconds available to operate once the cooldown period is fullfilled
+  uint256 public immutable OPERATE_WINDOW = 1 days;
+
+  address private tokenBaseDeployer; // staking erc20 tokens to mint PASS
+  address private nftBaseDeployer; // staking erc721 tokens to mint PASS
+  address private fixedPeriodDeployer; // pay erc20 tokens to mint PASS in a fixed period with linearly decreasing price
+  address private fixedPriceDeployer; // pay erc20 tokens to mint PASS with fixed price
+
+  uint256 public cooldownStartTimestamp;
+  address payable public platform; // The PASS platform commission account
+  uint256 public platformRate; // The PASS platform commission rate in pph
 
   constructor(
-    address _tokenBaseDeployer,         
+    address _tokenBaseDeployer,
     address _nftBaseDeployer,
     address _fixedPeriodDeployer,
-    address _fixedPriceDeployer
+    address _fixedPriceDeployer,
+    address payable _platform,
+    uint256 _platformRate
   ) Ownable(msg.sender) {
     tokenBaseDeployer = _tokenBaseDeployer;
     nftBaseDeployer = _nftBaseDeployer;
     fixedPeriodDeployer = _fixedPeriodDeployer;
     fixedPriceDeployer = _fixedPriceDeployer;
+    _setPlatformParms(_platform, _platformRate);
   }
 
   event TokenBaseDeploy(
-    address indexed _addr,   // address of deployed NFT PASS contract
-    string _name,            // name of PASS
-    string _symbol,          // symbol of PASS
-    string _bURI,            // baseuri of NFT PASS
-    address _erc20,          // address of staked erc20 tokens
-    uint256 _rate            // staking rate of erc20 tokens/PASS
+    address indexed _addr, // address of deployed NFT PASS contract
+    string _name, // name of PASS
+    string _symbol, // symbol of PASS
+    string _bURI, // baseuri of NFT PASS
+    address _erc20, // address of staked erc20 tokens
+    uint256 _rate // staking rate of erc20 tokens/PASS
   );
   event NFTBaseDeploy(
     address indexed _addr,
     string _name,
     string _symbol,
     string _bURI,
-    address _erc721          // address of staked erc721 tokens
+    address _erc721 // address of staked erc721 tokens
   );
   event FixedPeriodDeploy(
     address indexed _addr,
     string _name,
     string _symbol,
     string _bURI,
-    address _erc20,          // payment erc20 tokens
+    address _erc20, // payment erc20 tokens
     address _platform,
-    address _beneficiary,    // creator's beneficiary account to receive erc20 tokens
-    uint256 _initialRate,    // initial exchange rate of erc20 tokens/PASS
-    uint256 _startTime,      // start time of sales period
-    uint256 _endTime,        // start time of sales
-    uint256 _maxSupply,      // maximum supply of PASS
-    uint256 _platformRate    
+    address _beneficiary, // creator's beneficiary account to receive erc20 tokens
+    uint256 _initialRate, // initial exchange rate of erc20 tokens/PASS
+    uint256 _startTime, // start time of sales period
+    uint256 _endTime, // start time of sales
+    uint256 _maxSupply, // maximum supply of PASS
+    uint256 _platformRate
   );
 
   event FixedPriceDeploy(
@@ -63,7 +72,7 @@ contract Factory is Ownable {
     string _name,
     string _symbol,
     string _bURI,
-    address _erc20,           // payment erc20 tokens
+    address _erc20, // payment erc20 tokens
     address _platform,
     address _beneficiary,
     uint256 _rate,
@@ -71,16 +80,49 @@ contract Factory is Ownable {
     uint256 _platformRate
   );
 
-  event SetPlatformParm(address _platform, uint256 _platformRate);
+  event SetPlatformParms(address _platform, uint256 _platformRate);
+  event SetPlatformParmsUnlock(uint256 cooldownStartTimestamp);
+
+  // unlock setPlatformParms function
+  function setPlatformParmsUnlock() public onlyOwner {
+    cooldownStartTimestamp = block.timestamp;
+    emit SetPlatformParmsUnlock(block.timestamp);
+  }
 
   // set the platform account and commission rate, only operable by contract owner, _platformRate is in pph
   function setPlatformParm(address payable _platform, uint256 _platformRate)
     public
     onlyOwner
   {
+    require(
+      block.timestamp > cooldownStartTimestamp + COOLDOWN_SECONDS,
+      "INSUFFICIENT_COOLDOWN"
+    );
+    require(
+      block.timestamp - (cooldownStartTimestamp + COOLDOWN_SECONDS) <=
+        OPERATE_WINDOW,
+      "OPERATE_WINDOW_FINISHED"
+    );
+
+    _setPlatformParms(_platform, _platformRate);
+
+    // clear cooldown after changeBeneficiary
+    if (cooldownStartTimestamp != 0) {
+      cooldownStartTimestamp = 0;
+    }
+  }
+
+  // set up the platform parameters internal
+  function _setPlatformParms(address payable _platform, uint256 _platformRate)
+    internal
+  {
+    require(_platform != address(0), "Curve: platform address is zero.");
+    require(_platformRate <= 100, "Curve: wrong rate");
+
     platform = _platform;
     platformRate = _platformRate;
-    emit SetPlatformParm(_platform, _platformRate);
+
+    emit SetPlatformParms(_platform, _platformRate);
   }
 
   function tokenBaseDeploy(
@@ -92,7 +134,13 @@ contract Factory is Ownable {
   ) public {
     ITokenBaseDeployer factory = ITokenBaseDeployer(tokenBaseDeployer);
     //return the address of deployed NFT PASS contract
-    address addr = factory.deployTokenBase(_name, _symbol, _bURI, _erc20, _rate);
+    address addr = factory.deployTokenBase(
+      _name,
+      _symbol,
+      _bURI,
+      _erc20,
+      _rate
+    );
     emit TokenBaseDeploy(addr, _name, _symbol, _bURI, _erc20, _rate);
   }
 
